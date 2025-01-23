8fa6c8ade267466b13c3aea1098552a3afc56689
Knight <knight@localhost.localdomain>
2025-01-23 22:50:16 +0600

    new repo

--- src/Sorting/bubblesort/bubblesort.java
+++ src/Sorting/bubblesort/bubblesort.java
@@ -1,4 +1,24 @@
 package Sorting.bubblesort;
 
 public class bubblesort {
+    public static void main(String[] args) {
+        int[] arr = {3,2,1,5,3,7,34,12};
+        bubble(arr);
+        for(int i : arr){
+            System.out.print(i + "  ");
+        }
+
+    }
+
+    private static void bubble(int[] arr) {
+        for(int i = 0; i < arr.length -1; i++){
+            for (int j = 0; j < arr.length - i - 1; j++){
+                if (arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
